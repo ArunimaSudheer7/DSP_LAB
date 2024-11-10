87b1a8186cf7303ee2acc256f9990cea40197bc6
ArunimaSudheer7 <122149117+ArunimaSudheer7@users.noreply.github.com>
2024-11-10 20:30:45 +0530

    Create Linear Convolution using Circular Convolution.m
    a) To perform Linear Convolution using Circular Convolution.

---
+++ Experiment5-Linear Convolution using Circular Convolution and vice versa/Linear Convolution using Circular Convolution.m
@@ -0,0 +1,15 @@
+clc; 
+close all; 
+clear all; 
+x = [1 2 3 4];
+h = [1 1 1 ]; 
+l=length(x); 
+m=length(h); 
+n=l+m-1; 
+x=[x zeros(1,n-l)]; 
+h=[h zeros(1,n-m)]; 
+x1=fft(x); 
+h1=fft(h); 
+y1=x1.*h1; 
+y=ifft(y1); 
+disp(y);
